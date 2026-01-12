d25a11f7205564c361fbc5ec635eee40ee4ad0b9
lv5207 <lv5207@bs.amalnet.k12.il>
2026-01-12 16:29:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex0606/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ex0606;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
